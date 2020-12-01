1e4a1960c11702a6e6efb2e923a56b337620dc33
kishore123155 <you@example.com>
2020-12-01 22:28:15 +0530

    adding prin.sh file

--- prin.sh
+++ prin.sh
@@ -11,3 +11,11 @@ echo Hello World
 echo line1
 echo line2
 
+# Print two lines single echo command
+# This can be done with helop of escape sequences
+
+# \n - new line
+# \t - new tab
+# \e - new color
+
+echo -e "Line11\nLine22"
