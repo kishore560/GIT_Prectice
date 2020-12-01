a2ec7ae2e2ee6e47254faa7f2275a66f9b6e9182
kishore123155 <you@example.com>
2020-12-01 22:20:07 +0530

    adding prin.sh file

---
+++ prin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# echo and printf are two commands can help in printing.
+# we use echo command because  it is more user friendly, yet supports all the requirements.
+
+echo Hello World
+
+#
