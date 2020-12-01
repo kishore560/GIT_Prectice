bb03a29554926e10e3ed5e5df174c82af37a236a
kishore123155 <you@example.com>
2020-12-01 23:48:05 +0530

    adding 03-variables.sh file

---
+++ Shell-scrpting/Shell-scrpting/03-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# variable will help you in times of replacing the value.
+
+echo Hello Kishore, Welcome traning
+echo DevOps traning timingd are 6AM IST
+echo DevOps traning course covers shell scrpting
+
+## Declare variable
+a=10 # Number
+aa=xyz # characters
+aaa=abc123 # string
+a1=1.90 # Float
+
+# Observation, shell does not bother about data types by default. Everything is a string
+
+## Access the variable
+# Print the data using echo command
+echo a = $a
+
