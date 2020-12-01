788b597f5b781860105bf5f130699be5fdc783b0
kishore123155 <you@example.com>
2020-12-01 22:32:07 +0530

    adding prin.sh file

--- prin.sh
+++ prin.sh
@@ -19,3 +19,11 @@ echo line2
 # \e - new color
 
 echo -e "Line11\nLine22"
+
+# Observations:
+# 1. You should use -e option when use escape sequences, It is a kind of enabling esc seq
+# 2. Input has to be in quotes, double quotes is preferred
+
+# Tab space is printed by \t
+
+echi -e "New\t\t\tWorld"
