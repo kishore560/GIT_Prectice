28e8d613d2d8aedd076d5b9ba216f88f83c56416
kishore123155 <you@example.com>
2020-12-02 14:09:56 +0530

    adding roboshop.sh file

--- Shell-scrpting/Shell-scrpting/roboshop.sh
+++ Shell-scrpting/Shell-scrpting/roboshop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo -e "\t\t\e[1;4;33msetting up Frontend\e[0m"
+echo "Installing Nginx"
 yum install nginx -y
 
 systemctl enable nginx
@@ -8,3 +9,7 @@ systemctl enable nginx
 systemctl start nginx
 echo -e "\t\t\e[1;4;33msetting up Catalogue\e[0m"
 echo -e "\t\t\e[1;4;33msetting up Cart\e[0m"
+
+# Enchancements
+
+# 1. Commands output
