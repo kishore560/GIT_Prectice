9402132671617e427ea5f65da1fdbf271815ab6a
kishore123155 <you@example.com>
2020-12-04 23:25:26 +0530

    adding prin.sh file

--- prin.sh
+++ prin.sh
@@ -48,3 +48,18 @@ echo $BIGEXPRESSION
 read -p 'Enter your name: ' name
 
 echo "your name = $name"
+
+#!/bin/bash
+
+# Reading the input from arguments can be done through special variables.
+# Special Variables for inputs are , $0 - $n , $*, $@, $#
+
+echo First Argument = $1
+echo Second Argument = $2
+
+echo Name of the Script =  $0
+
+echo All Arguments = $*
+echo All Arguments = $@
+
+echo Number of Arguments =  $#
